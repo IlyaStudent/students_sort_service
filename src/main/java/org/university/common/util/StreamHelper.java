8f5401223c1b5b328305043bf26d9c8d590169e3
IlyaStudent <ilyaprepodavatel@gmail.com>
2025-12-17 16:58:22 +0300

    util classes

---
+++ src/main/java/org/university/common/util/StreamHelper.java
@@ -0,0 +1,7 @@
+package org.university.common.util;
+
+public final class StreamHelper {
+    private StreamHelper() {
+        throw new AssertionError("Cannot instantiate StreamHelper class");
+    }
+}
