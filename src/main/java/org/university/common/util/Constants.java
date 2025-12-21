be48bb06f2ccf72c1b4bc959fa08d6c9e017b037
Chippendron <feedback.medentsov@gmail.com>
2025-12-21 18:29:08 +0300

    Constants update

--- src/main/java/org/university/common/util/Constants.java
+++ src/main/java/org/university/common/util/Constants.java
@@ -3,6 +3,11 @@ package org.university.common.util;
 public final class Constants {
 
     public static final String APP_NAME = "Student Sorting System";
+    public static final String JSON_FILENAME = "students.json";
+    public static final String[] GROUP_PREFIXES = {
+            "CS", "IT", "SE", "AI", "CY", "MA", "PH", "BI", "EC", "EN",
+            "CH", "PH", "ME", "EE", "CE", "BT", "MT", "AT", "NE", "RO"
+    };
 
     private Constants() {
         throw new AssertionError("Cannot instantiate a Constants class");
