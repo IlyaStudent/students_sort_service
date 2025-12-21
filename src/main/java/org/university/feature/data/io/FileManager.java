e8db35c213cd933155826d0fd502b6127668d7e6
Chippendron <feedback.medentsov@gmail.com>
2025-12-21 18:35:15 +0300

    fixed error in regex

--- src/main/java/org/university/feature/data/io/FileManager.java
+++ src/main/java/org/university/feature/data/io/FileManager.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class FileManager {
 
     private static final Pattern JSON_FILE_PATTERN =
-            Pattern.compile("^[a-zA-Z0-9_\\-\\.\\s]+?\\.json$", Pattern.CASE_INSENSITIVE);
+            Pattern.compile("^[a-zA-Z0-9_.\\-\\s]+?\\.json$", Pattern.CASE_INSENSITIVE);
     private static final String JSON_FILEPATH = "src/main/resources/data/";
 
     public static String getJSON_Filepath(String fileName) {
