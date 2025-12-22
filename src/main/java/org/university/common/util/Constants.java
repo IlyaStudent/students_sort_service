10c817da74d96e6088d83e20bb9a1feab3bbbbc9
Chippendron <feedback.medentsov@gmail.com>
2025-12-22 14:30:43 +0300

    upadted constants

--- src/main/java/org/university/common/util/Constants.java
+++ src/main/java/org/university/common/util/Constants.java
@@ -1,5 +1,7 @@
 package org.university.common.util;
 
+import java.util.regex.Pattern;
+
 public final class Constants {
 
     public static final String APP_NAME = "Student Sorting System";
@@ -8,6 +10,12 @@ public final class Constants {
             "CS", "IT", "SE", "AI", "CY", "MA", "PH", "BI", "EC", "EN",
             "CH", "PH", "ME", "EE", "CE", "BT", "MT", "AT", "NE", "RO"
     };
+    public static final Pattern JSON_FILE_PATTERN =
+            Pattern.compile("^[a-zA-Z0-9_.\\-\\s]+?\\.json$");
+    public static final String JSON_FILEPATH = "src/main/resources/data/";
+
+    public static final double MIN_AVERAGE_SCORE  = 0.0;
+    public static final double MAX_AVERAGE_SCORE  = 5.0;
 
     private Constants() {
         throw new AssertionError("Cannot instantiate a Constants class");
