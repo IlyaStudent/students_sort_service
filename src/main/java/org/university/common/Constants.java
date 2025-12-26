7920ddadcfddcbb566977c3996e7b1453a9f7b4a
ARKA99 <155990363+Arkady465@users.noreply.github.com>
2025-12-26 19:41:25 +0300

    Delete src directory

--- src/main/java/org/university/common/Constants.java
+++
@@ -1,9 +0,0 @@
-package org.university.common;
-
-public final class Constants {
-
-    private Constants() {
-    }
-
-    public static final int DEFAULT_CAPACITY = 10;
-}
--- src/main/java/org/university/common/util/StreamHelper.java
+++
@@ -1,7 +0,0 @@
-package org.university.common.util;
-
-public final class StreamHelper {
-    private StreamHelper() {
-        throw new AssertionError("Cannot instantiate StreamHelper class");
-    }
-}
